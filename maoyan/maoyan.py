a18af7e86cbe4bfd13544a7e212fdada145fb152
shanshanya123 <44665764+shanshanya123@users.noreply.github.com>
2019-02-13 15:47:44 +0800

    Update maoyan.py

--- maoyan/maoyan.py
+++ maoyan/maoyan.py
@@ -1,4 +1,5 @@
 import requests
+import re
 def get_one_page(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'
@@ -12,3 +13,19 @@ def main():
     html = get_one_page(url)
     print(html)
 main()
+
+def parse_one_page(html):
+     pattern = re.compile(
+         '<dd>.*?board-index.*?>(.*?)</i>.*?data-src="(.*?)".*?name.*?a.*?>(.*?)</a>.*?star.*?>(.*?)</p>.'
+         '*?releasetime.*?>(.*?)</p>.*?integer.*?>(.*?)</i>.*?fraction.*?>(.*?)</i>.*?</dd>',
+         re.S)
+     items = re.findall(pattern,html)
+     for item in items:
+       yield {
+           'index':item[0],
+           'image':item[1],
+           'title':item[2].strip(),
+           'actor':item[3].strip()[3:] if len(item[3])>3 else '',
+           'time':item[4].strip()[5:]if len(item[4])>5 else '',
+           'score':item[5].strip()+item[6].strip()
+       }
